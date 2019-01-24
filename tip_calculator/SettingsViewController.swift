fe3b6bf8ba4100a51ba0194a0cbf9822c0bbf709
resteybar <resteybar@csumb.edu>
2019-01-23 21:49:46 -0800

    Added Navigation from Home to Settings

---
+++ tip_calculator/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  tip_calculator
+//
+//  Created by Raymond Esteybar on 1/23/19.
+//  Copyright © 2019 Raymond Esteybar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func on_tap(_ sender: Any) {
+        print("Tapped in Settings")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
