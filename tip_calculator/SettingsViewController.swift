2bed476a17e079446c2e579c272d94eee5514f5f
resteybar <resteybar@csumb.edu>
2019-01-24 16:38:22 -0800

    Fixing error: Tip uses incorrect value to calculate itself from settings

--- tip_calculator/SettingsViewController.swift
+++ tip_calculator/SettingsViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var percentage_field: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func on_tap(_ sender: Any) {
-        print("Tapped in Settings")
+    @IBAction func change_default_percentage(_ sender: Any) {
+        // New
+        // Using notification to store new default percentage
+        let new_default = Int(percentage_field.text!) ?? 10
+        var dict: Dictionary<String, AnyObject> = Dictionary()
+        dict.updateValue(new_default as AnyObject, forKey: "default_percentage")
+        
+        NotificationCenter.default.post(name: Notification.Name("tip_notification"), object: nil, userInfo: dict)
+        
+//        _ = self.navigationController?.popViewController(animated: true)
+        
+        // Old
+        // Access UserDefaults
+        let defaults = UserDefaults.standard
+        
+        defaults.set(Int(percentage_field.text!) ?? 10, forKey: "default_percentage")
+        
+        // Force UserDefaults to save.
+        defaults.synchronize()
     }
+    
     
     /*
     // MARK: - Navigation
--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -17,14 +17,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = UserDefaults.standard
+        let default_tip = defaults.integer(forKey: "default_percentage")
+        tip_control.setTitle(String(format: "%d%%", default_tip), forSegmentAt: 0)
+        
+        // * Used to Automatically update the default tip from different view
+        //  - Tutorial used: https://www.youtube.com/watch?v=iztROUzjpM0
+        NotificationCenter.default.addObserver(self, selector: #selector(notification_fired(_:)), name: Notification.Name("tip_notification"), object: nil)
+    }
+    
+    // *
+    @objc func notification_fired(_ notification: Notification) {
+        let default_info = notification.userInfo
+        
+        if let new_perc = default_info?["default_percentage"] as? Int {
+            tip_control.setTitle(String(format: "%d%%", new_perc), forSegmentAt: 0)
+            
+            let bill = Double(bill_field.text!) ?? 0
+            
+            let defaults = UserDefaults.standard
+            let default_tip = defaults.integer(forKey: "default_percentage")
+            let dec_default = Double(default_tip) / 100
+            
+            // Calculate tip & total
+            let tip_percentages = [dec_default, 0.15, 0.2]
+            let tip = bill * tip_percentages[tip_control.selectedSegmentIndex]
+            let total = bill + tip
+            
+            print("DTip: \(default_tip)")
+            print(" Tip: \(tip)\n")
+            
+            // Update tip & total labels
+            tip_label.text = String(format: "$%.2f", tip)
+            total_label.text = String(format: "$%.2f", total)
+        }
     }
 
     @IBAction func calculate_tip(_ sender: Any) {
         // Get bill amount
         let bill = Double(bill_field.text!) ?? 0
-            
+        
+        let defaults = UserDefaults.standard
+        let default_tip = defaults.integer(forKey: "default_percentage")
+        let dec_default = Double(default_tip) / 100
+        
         // Calculate tip & total
-        let tip_percentages = [0.1, 0.15, 0.2]
+        let tip_percentages = [dec_default, 0.15, 0.2]
         let tip = bill * tip_percentages[tip_control.selectedSegmentIndex]
         let total = bill + tip
         
